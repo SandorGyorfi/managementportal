85d82df7c8ecdc3c3d871fccbf1dd1ec4a30e393
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-10-26 16:30:40 +0100

    Add
    Added Payment model with customer reference and status in financial_department app.

--- financial_department/models.py
+++ financial_department/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from customer_service.models import Customer
+
 
 # Create your models here.
+class Payment(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    STATUS_CHOICES = [
+        ('Pending Payment', 'Pending Payment'),
+        ('Payment Processed', 'Payment Processed'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending Payment')
+    stripe_bank_details = models.TextField(blank=True, null=True)
