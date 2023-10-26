9bbd63a6c4a03ba400f170e6bfb50a7920f0048b
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-10-26 16:25:41 +0100

    Add
    Added view to display pending tasks for head office.

--- head_office/views.py
+++ head_office/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from customer_service.models import Customer
+from financial_department.models import Payment
+from delivery_department.models import Delivery
+from django.db.models import F
+from datetime import date
 
 # Create your views here.
+@login_required(login_url='operator_login')
+def view_head_office(request):
+    pending_customers = Customer.objects.all()
+    pending_payments = Payment.objects.filter(status="Pending Payment")
+    out_for_deliveries = Delivery.objects.filter(status="Pending Delivery")
+    overdue_deliveries = Delivery.objects.filter(delivery_date__lt=date.today()).exclude(status="Delivered")
+
+    context = {
+        'pending_customers': pending_customers,
+        'pending_payments': pending_payments,
+        'out_for_deliveries': out_for_deliveries,
+        'overdue_deliveries': overdue_deliveries,
+        'today': date.today(),
+    }
+
+    return render(request, 'head_office/head_office.html', context)
