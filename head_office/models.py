aad09d83fc86e6b6fb060eccf798b2d9807de2fd
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-10-26 16:23:51 +0100

    Add
    Added custom user model 'DepartmentUser' with department choices.

--- head_office/models.py
+++ head_office/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager as DefaultUserManager
+
 
 # Create your models here.
+class UserManager(DefaultUserManager):
+    pass
+
+class DepartmentUser(AbstractUser):
+    DEPARTMENTS = (
+        ('head_office', 'Head Office'),
+        ('customer_service', 'Customer Service'),
+        ('financial_department', 'Financial Department'),
+        ('delivery_department', 'Delivery Department'),
+    )
+    
+    department = models.CharField(choices=DEPARTMENTS, max_length=50)
+    objects = UserManager()
