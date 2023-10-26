7eb5194f4650c176ca821527d850a5a95603df90
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-10-26 16:58:01 +0100

    Add
    Resolved reverse accessor clash in DepartmentUser model.

--- head_office/models.py
+++ head_office/models.py
@@ -1,11 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager as DefaultUserManager
-
-
+from django.contrib.auth.models import AbstractUser, UserManager as DefaultUserManager, Group, Permission
 # Create your models here.
-class UserManager(DefaultUserManager):
-    pass
-
 class DepartmentUser(AbstractUser):
     DEPARTMENTS = (
         ('head_office', 'Head Office'),
@@ -15,4 +10,20 @@ class DepartmentUser(AbstractUser):
     )
     
     department = models.CharField(choices=DEPARTMENTS, max_length=50)
-    objects = UserManager()
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name="departmentuser_set",
+        related_query_name="departmentuser",
+        help_text='The groups this department user belongs to. A user will get all permissions granted to each of their groups.'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name="departmentuser_set",
+        related_query_name="departmentuser",
+        help_text='Specific permissions for this department user.'
+    )
+    objects = DefaultUserManager()
