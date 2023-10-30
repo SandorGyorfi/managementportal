02f16026c166aca4f851d4fef157d3ba34fa96fd
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-10-30 17:56:47 +0000

    Update
    Corrected view.

--- head_office/views.py
+++ head_office/views.py
@@ -36,26 +36,43 @@ def view_head_office(request):
 
 @login_required(login_url='operator_login')
 def delete_order(request, customer_id):
-    if request.method == 'POST':
-        operator_name = request.POST.get('operator_name')
-        pin_code = request.POST.get('pin_code')
 
-        try:
-            user = DepartmentUser.objects.get(username=operator_name)
-
-
-            correct_pin = DEPARTMENT_MAP.get("head_office")[1]
-
-            if pin_code == correct_pin:
-                customer = Customer.objects.get(id=customer_id)
-                customer.delete()
-                messages.success(request, 'Order successfully deleted!')
-            else:
-                messages.error(request, 'Incorrect pin code!')
-
-        except DepartmentUser.DoesNotExist:
-            messages.error(request, 'Invalid operator name')
-
+    if request.method != 'POST':
+        print("Not a POST request.")
         return redirect(reverse('head_office:view_head_office'))
+
+    operator_name = request.POST.get('operator_name')
+    pin_code = request.POST.get('pin_code')
+
+    if not operator_name or not pin_code:
+        print(f"operator_name: {operator_name}, pin_code: {pin_code}")
+        messages.error(request, 'Operator name or pin code missing!')
+        return redirect(reverse('head_office:view_head_office'))
+
+    try:
+        user = DepartmentUser.objects.get(username=operator_name)
+    except DepartmentUser.DoesNotExist:
+        print(f"User with name {operator_name} does not exist.")
+        messages.error(request, 'Invalid operator name')
+        return redirect(reverse('head_office:view_head_office'))
+
+    correct_pin = DEPARTMENT_MAP.get("head_office")[1]
+
+    if pin_code != correct_pin:
+        print(f"Entered pin code: {pin_code} does not match correct pin.")
+        messages.error(request, 'Incorrect pin code!')
+        return redirect(reverse('head_office:view_head_office'))
+
+    try:
+        customer = Customer.objects.get(id=customer_id)
+        customer.delete()
+        messages.success(request, 'Order successfully deleted!')
+    except Customer.DoesNotExist:
+        print(f"Customer with ID: {customer_id} does not exist.")
+        messages.error(request, 'Invalid customer ID!')
+    except Exception as e: 
+        # Catch any other unexpected exceptions
+        print(f"Unexpected error: {str(e)}")
+        messages.error(request, 'An unexpected error occurred!')
 
     return redirect(reverse('head_office:view_head_office'))
