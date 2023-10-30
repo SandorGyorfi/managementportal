448032bb950cd543f014ac8b131849abc22f5ccd
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-10-30 16:34:33 +0000

    Update
    Delete function added to head_office app. Also updated the required files.

--- head_office/urls.py
+++ head_office/urls.py
@@ -5,4 +5,5 @@ app_name = 'head_office'
 
 urlpatterns = [
     path('head_office/', views.view_head_office, name='head_office'),
+    path('delete_order/<int:customer_id>/', views.delete_order, name='delete_order'),
 ]
--- head_office/views.py
+++ head_office/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from customer_service.models import Customer
 from financial_department.models import Payment
 from delivery_department.models import Delivery
 from django.db.models import F
 from datetime import date
+from django.contrib import messages
+from decouple import config  
+from head_office.models import DepartmentUser
 
-# Create your views here.
 @login_required(login_url='operator_login')
 def view_head_office(request):
     pending_customers = Customer.objects.all()
@@ -23,3 +25,34 @@ def view_head_office(request):
     }
 
     return render(request, 'head_office/head_office.html', context)
+
+@login_required(login_url='operator_login')
+def delete_order(request, customer_id):
+    if request.method == 'POST':
+        operator_name = request.POST.get('operator_name')
+        pin_code = request.POST.get('pin_code')
+
+        try:
+            user = DepartmentUser.objects.get(username=operator_name)
+            DEPARTMENT_MAP = {
+                "customer_service": (config('CS_OPERATOR_USERNAME', default=''), config('CS_OPERATOR_PIN', default='')),
+                "financial_department": (config('FD_OPERATOR_USERNAME', default=''), config('FD_OPERATOR_PIN', default='')),
+                "delivery_department": (config('DD_OPERATOR_USERNAME', default=''), config('DD_OPERATOR_PIN', default='')),
+                "head_office": (config('HO_OPERATOR_USERNAME', default=''), config('HO_OPERATOR_PIN', default=''))
+            }
+            
+            correct_pin = DEPARTMENT_MAP.get("head_office")[1]
+
+            if pin_code == correct_pin:
+                customer = Customer.objects.get(id=customer_id)
+                customer.delete()
+                messages.success(request, 'Order successfully deleted!')
+            else:
+                messages.error(request, 'Incorrect pin code!')
+
+        except DepartmentUser.DoesNotExist:
+            messages.error(request, 'Invalid operator name')
+
+        return redirect(reverse('head_office:view_head_office'))
+
+    return redirect(reverse('head_office:view_head_office'))
