4cfe091c0e773fe3318161c079b5852383f90354
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-10-30 18:38:29 +0000

    Update
    Reversed back to original.

--- customer_service/views.py
+++ customer_service/views.py
@@ -7,19 +7,21 @@ from head_office.models import DepartmentUser
 
 # Create your views here.
 
-DEPARTMENT_MAP = {
-    "customer_service": (config('CS_OPERATOR_USERNAME', default=''), config('CS_OPERATOR_PIN', default='')),
-    "financial_department": (config('FD_OPERATOR_USERNAME', default=''), config('FD_OPERATOR_PIN', default='')),
-    "delivery_department": (config('DD_OPERATOR_USERNAME', default=''), config('DD_OPERATOR_PIN', default='')),
-    "head_office": (config('HO_OPERATOR_USERNAME', default=''), config('HO_OPERATOR_PIN', default=''))
-}
-
 def operator_login(request):
     error = None
     if request.method == 'POST':
         department = request.POST.get('department')
         username = request.POST.get('username')
         pin = request.POST.get('password')
+
+        DEPARTMENT_MAP = {
+            "customer_service": (config('CS_OPERATOR_USERNAME', default=''), config('CS_OPERATOR_PIN', default='')),
+            "financial_department": (config('FD_OPERATOR_USERNAME', default=''), config('FD_OPERATOR_PIN', default='')),
+            "delivery_department": (config('DD_OPERATOR_USERNAME', default=''), config('DD_OPERATOR_PIN', default='')),
+            "head_office": (config('HO_OPERATOR_USERNAME', default=''), config('HO_OPERATOR_PIN', default=''))
+        }
+
+        
 
         if department not in DEPARTMENT_MAP:
             error = 'Invalid department'
@@ -29,9 +31,9 @@ def operator_login(request):
                 user.backend = 'django.contrib.auth.backends.ModelBackend'
             login(request, user)
             if user.is_authenticated:
-                request.session['just_logged_in'] = True
+                request.session['just_logged_in'] = True  
                 if department == 'financial_department':
-                    return redirect('financial_department:view_payments')
+                    return redirect('financial_department:view_payments')  
                 elif department == 'customer_service':
                     return redirect('add_customer')
                 elif department == 'delivery_department':
@@ -43,13 +45,13 @@ def operator_login(request):
                 error = 'Unable to authenticate user'
         else:
             error = 'Invalid credentials'
-            
+
     return render(request, 'login.html', {'error': error})
 
 @login_required(login_url='operator_login')
 def add_customer(request):
     context = {}
-
+    
     if request.session.pop('just_logged_in', False):
         context['just_logged_in'] = True
 
@@ -60,7 +62,7 @@ def add_customer(request):
             from financial_department.models import Payment
             Payment.objects.create(customer=customer, status="Pending Payment")
             context['message_display'] = True
-            form = CustomerForm()
+            form = CustomerForm()  
         else:
             context['form'] = form
     else:
--- head_office/views.py
+++ head_office/views.py
@@ -1,22 +1,12 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from customer_service.models import Customer
 from financial_department.models import Payment
 from delivery_department.models import Delivery
 from django.db.models import F
 from datetime import date
-from django.contrib import messages
-from decouple import config
-from head_office.models import DepartmentUser
 
-
-DEPARTMENT_MAP = {
-    "customer_service": (config('CS_OPERATOR_USERNAME', default=''), config('CS_OPERATOR_PIN', default='')),
-    "financial_department": (config('FD_OPERATOR_USERNAME', default=''), config('FD_OPERATOR_PIN', default='')),
-    "delivery_department": (config('DD_OPERATOR_USERNAME', default=''), config('DD_OPERATOR_PIN', default='')),
-    "head_office": (config('HO_OPERATOR_USERNAME', default=''), config('HO_OPERATOR_PIN', default=''))
-}
-
+# Create your views here.
 @login_required(login_url='operator_login')
 def view_head_office(request):
     pending_customers = Customer.objects.all()
@@ -33,46 +23,3 @@ def view_head_office(request):
     }
 
     return render(request, 'head_office/head_office.html', context)
-
-@login_required(login_url='operator_login')
-def delete_order(request, customer_id):
-
-    if request.method != 'POST':
-        print("Not a POST request.")
-        return redirect(reverse('head_office:view_head_office'))
-
-    operator_name = request.POST.get('operator_name')
-    pin_code = request.POST.get('pin_code')
-
-    if not operator_name or not pin_code:
-        print(f"operator_name: {operator_name}, pin_code: {pin_code}")
-        messages.error(request, 'Operator name or pin code missing!')
-        return redirect(reverse('head_office:view_head_office'))
-
-    try:
-        user = DepartmentUser.objects.get(username=operator_name)
-    except DepartmentUser.DoesNotExist:
-        print(f"User with name {operator_name} does not exist.")
-        messages.error(request, 'Invalid operator name')
-        return redirect(reverse('head_office:view_head_office'))
-
-    correct_pin = DEPARTMENT_MAP.get("head_office")[1]
-
-    if pin_code != correct_pin:
-        print(f"Entered pin code: {pin_code} does not match correct pin.")
-        messages.error(request, 'Incorrect pin code!')
-        return redirect(reverse('head_office:view_head_office'))
-
-    try:
-        customer = Customer.objects.get(id=customer_id)
-        customer.delete()
-        messages.success(request, 'Order successfully deleted!')
-    except Customer.DoesNotExist:
-        print(f"Customer with ID: {customer_id} does not exist.")
-        messages.error(request, 'Invalid customer ID!')
-    except Exception as e: 
-        # Catch any other unexpected exceptions
-        print(f"Unexpected error: {str(e)}")
-        messages.error(request, 'An unexpected error occurred!')
-
-    return redirect(reverse('head_office:view_head_office'))
