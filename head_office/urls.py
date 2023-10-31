0e556c8f2acc3f0c87cb3985a1996ac4c57ca016
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-10-31 09:54:11 +0000

    Update
    Updated urls refers to the error occured.

--- head_office/urls.py
+++ head_office/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'head_office'
 
 urlpatterns = [
-    path('head_office/', views.view_head_office, name='head_office'),
+    path('head_office/', views.view_head_office, name='view_head_office'),
     path('delete_payment/<int:payment_id>/', views.delete_payment, name='delete_payment'),
     path('delete_delivery/<int:delivery_id>/', views.delete_delivery, name='delete_delivery'),
-
 ]
